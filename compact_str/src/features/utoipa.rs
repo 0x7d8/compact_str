7d4839e98f683241ce49ec8a34be0de4e39a8a26
Robert Jansen <me@rjns.dev>
2025-12-21 14:57:56 +0100

    better utoipa impl

--- compact_str/src/features/utoipa.rs
+++ compact_str/src/features/utoipa.rs
@@ -1,27 +1,21 @@
 use crate::CompactString;
 
 #[cfg_attr(docsrs, doc(cfg(feature = "utoipa")))]
-impl utoipa::PartialSchema for CompactString {
-    #[inline]
-    fn schema() -> utoipa::openapi::RefOr<utoipa::openapi::schema::Schema> {
-        std::string::String::schema()
+impl utoipa::__dev::ComposeSchema for CompactString {
+    fn compose(
+        new_generics: std::vec::Vec<utoipa::openapi::RefOr<utoipa::openapi::schema::Schema>>,
+    ) -> utoipa::openapi::RefOr<utoipa::openapi::schema::Schema> {
+        std::string::String::compose(new_generics)
     }
 }
 
-#[cfg_attr(docsrs, doc(cfg(feature = "utoipa")))]
-impl utoipa::ToSchema for CompactString {
-    #[inline]
-    fn name() -> std::borrow::Cow<'static, str> {
-        std::string::String::name()
-    }
-
-    #[inline]
-    fn schemas(
-        schemas: &mut std::vec::Vec<(
-            std::string::String,
-            utoipa::openapi::RefOr<utoipa::openapi::schema::Schema>,
-        )>,
-    ) {
-        std::string::String::schemas(schemas)
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_schema_type() {
+        assert_eq!(
+            utoipa::schema!(std::string::String),
+            utoipa::schema!(crate::CompactString)
+        );
     }
 }
